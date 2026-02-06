b45d2f9e771e8029228fe69acf08384290b72831
Khanh Tran <khanhtranngoccva@gmail.com>
2026-02-06 11:24:16 +0700

    fix: pub for mount config

--- tests/offset_test.rs
+++ tests/offset_test.rs
@@ -1,4 +1,4 @@
-use easy_fuser::prelude::*;
+use easy_fuser::{MountConfig, prelude::*};
 use easy_fuser::templates::{DefaultFuseHandler, mirror_fs::*};
 
 use std::fs::{self, File};
@@ -20,16 +20,8 @@ fn test_mirror_fs_file_offsets() {
     let mntpoint_clone = mntpoint.clone();
     let handle = std::thread::spawn(move || {
         let fs = MirrorFs::new(source_path.clone(), DefaultFuseHandler::new());
-        let config = {
-            let mut config = Config::default();
-            config.acl = fuser::SessionACL::Owner;
-            if cfg!(feature = "serial") {
-                config.n_threads = None;
-            } else {
-                config.n_threads = Some(4);
-            }
-            config.mount_options = vec![];
-            config
+        let config = MountConfig {
+            
         };
         mount(fs, &mntpoint_clone, &config).unwrap();
     });
